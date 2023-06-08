026d42f19ca37afd7532a6e5c9086dd4301a6822
Chartreuse <gogosapiens@gmail.com>
2023-06-08 20:01:42 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='carlo',
+    version='0.1.0',
+    description='Description of your package',
+    packages=find_packages(),
+    install_requires=[
+        'google-auth',
+        'google-api-python-client',
+        'openai',
+        'google-cloud-storage'
+    ],
+)
