8e3fdac63d9ceacd6c62ebd1657e23da05eee77d
gogosapiens <gogosapiens@iMac-Pro-gogosapiens.local>
2023-06-15 00:18:12 +0200

    tmp

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='carlo',
-    version='0.1.17',
+    version='0.1.18',
     description='Description of your package',
     packages=find_packages(),
     install_requires=[
