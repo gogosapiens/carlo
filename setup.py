663f29f1a5cb7cb75376a848e0a05cf7100951da
Chartreuse <gogosapiens@gmail.com>
2023-06-15 01:43:19 +0200

    tmp

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='carlo',
-    version='0.1.25',
+    version='0.1.26',
     description='Description of your package',
     packages=find_packages(),
     install_requires=[
