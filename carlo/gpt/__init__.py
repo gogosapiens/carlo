ddb311033d6154332b818ef4276f02a5e5a4bea7
gogosapiens <gogosapiens@iMac-Pro-gogosapiens.local>
2023-06-17 11:55:53 +0200

    Added translation

--- carlo/gpt/__init__.py
+++ carlo/gpt/__init__.py
@@ -22,8 +22,9 @@ def get_answer(prompt, model="gpt-3.5-turbo", temperature=1):
 		return get_answer(prompt)
 	return response.choices[0].message.content
 
-def get_json(prompt, repeat_count=3):
-	json_str = get_answer(prompt)
+def get_json(prompt, repeat_count=3, model="gpt-3.5-turbo", temperature=1):
+	prompt += "\nDon't add any extra text. Return only JSON."
+	json_str = get_answer(prompt, model=model, temperature=temperature)
 	try:
 		json_data = json.loads(json_str)
 		return json_data
@@ -31,6 +32,16 @@ def get_json(prompt, repeat_count=3):
 		printc("Error decoding GPT JSON:", e)
 		if repeat_count > 0:
 			printc("Repeating GPT request...")
-			return get_json(prompt, repeat_count=repeat_count-1)
+			return get_json(prompt, repeat_count=repeat_count-1, model=model, temperature=temperature)
 		else:
 			return None
+		
+def translate(text, target_language, model="gpt-3.5-turbo", note=""):
+	prompt = f"Translate text into language {target_language}. {note}\nText: {text}\n Translation put in json with key 'translation'."
+	output = get_json(prompt, model=model)
+	if output == None:
+		return None 
+	if "translation" in output:
+		return output["translation"]
+	else:   
+		return None
