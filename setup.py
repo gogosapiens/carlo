e2944e41db05d30bd6b83f3b78226d27ee30fc47
gogosapiens <gogosapiens@gmail.com>
2024-12-11 10:00:19 +0100

    1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='carlo',
-    version='0.1.70',
+    version='0.1.80',
     description='Description of your package',
     packages=find_packages(),
     install_requires=[
